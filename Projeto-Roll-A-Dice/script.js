83f44a950cc6774d7c2ca78ce6f1ff20f7888544
Raf-RM <rafael.r.mateus@gmail.com>
2023-06-14 19:46:12 -0300

    add funcionalidade mudando resultados

--- Projeto-Roll-A-Dice/script.js
+++ Projeto-Roll-A-Dice/script.js
@@ -6,39 +6,41 @@ const d12Dice = document.querySelector('#d12')
 const d20Dice = document.querySelector('#d20')
 const d100Dice = document.querySelector('#d100')
 
+const result = document.querySelector(".screen2 h2")
+
 d4Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 4))
+  randomNumber = Math.ceil(Math.random() * 4)
+  result.innerHTML = randomNumber
 } )
 
 d6Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 6))
+  randomNumber = Math.ceil(Math.random() * 6)
+  result.innerHTML = randomNumber
 } )
 
 d8Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 8))
+  randomNumber = Math.ceil(Math.random() * 8)
+  result.innerHTML = randomNumber
 } )
 
 d10Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 10))
+  randomNumber = Math.ceil(Math.random() * 10)
+  result.innerHTML = randomNumber
 } )
 
 d12Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 12))
+  randomNumber = Math.ceil(Math.random() * 12)
+  result.innerHTML = randomNumber
 } )
 
 d20Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 20))
+  randomNumber = Math.ceil(Math.random() * 20)
+  result.innerHTML = randomNumber
 } )
 
 d100Dice.addEventListener('click', function () {
-  randomNumber = Math.random()
-  console.log(Math.ceil(Math.random() * 100))
+  randomNumber = Math.ceil(Math.random() * 100)
+  result.innerHTML = randomNumber
 } )
 
 function rollADice(diceFaces) {
